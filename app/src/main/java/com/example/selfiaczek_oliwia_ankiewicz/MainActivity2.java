757cd12953899b262676469d847e270f2536c73e
snaketrap <b.sarnecki11@gmail.com>
2022-10-18 14:38:39 +0200

    Pozostalo tylko zrobic APK i filtry RGB na zdjecie

--- app/src/main/java/com/example/selfiaczek_oliwia_ankiewicz/MainActivity2.java
+++ app/src/main/java/com/example/selfiaczek_oliwia_ankiewicz/MainActivity2.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+
+import com.google.android.material.animation.DrawableAlphaProperty;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -18,11 +22,23 @@ public class MainActivity2 extends AppCompatActivity {
         Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         ImageView img = findViewById(R.id.img);
         img.setImageBitmap(bmp);
-        img.setRotationX(15);
-
         SeekBar rotate1 = findViewById(R.id.rotateX);
         SeekBar rotate2 = findViewById(R.id.rotateY);
+        SeekBar rotate = findViewById(R.id.rotate);
+        SeekBar opacity = findViewById(R.id.Opacity);
+        opacity.setProgress(255);
 
+
+        rotate.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {img.setRotation(seekBar.getProgress());}
+        });
 
         rotate1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
@@ -44,6 +60,17 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {img.setRotationY(seekBar.getProgress());}
+        });
+
+        opacity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {img.setAlpha(seekBar.getProgress());}
         });
     }
 }
