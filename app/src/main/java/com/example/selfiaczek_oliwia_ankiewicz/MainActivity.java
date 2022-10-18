682132f89e1616e5f2457baea5d9f7bc41888c96
snaketrap <b.sarnecki11@gmail.com>
2022-10-18 12:57:02 +0200

    ewidentnie cos nie tak, trzeba naniesc poprawki

--- app/src/main/java/com/example/selfiaczek_oliwia_ankiewicz/MainActivity.java
+++ app/src/main/java/com/example/selfiaczek_oliwia_ankiewicz/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.selfiaczek_oliwia_ankiewicz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView Imageview;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
+        Imageview = findViewById(R.id.img);
+        btn = findViewById(R.id.btn);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                Manifest.permission.CAMERA
+            },100);
+        }
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        if(requestCode == 100){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            Imageview.setImageBitmap(bitmap);
+        }
     }
 }
