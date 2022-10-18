b2df092b37160f28cc6d2fb50a2a66ea64802ae4
snaketrap <b.sarnecki11@gmail.com>
2022-10-18 13:59:42 +0200

    Rotacja przez seekbar (rotacja X, Y)

--- app/src/main/java/com/example/selfiaczek_oliwia_ankiewicz/MainActivity2.java
+++ app/src/main/java/com/example/selfiaczek_oliwia_ankiewicz/MainActivity2.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -17,5 +18,32 @@ public class MainActivity2 extends AppCompatActivity {
         Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         ImageView img = findViewById(R.id.img);
         img.setImageBitmap(bmp);
+        img.setRotationX(15);
+
+        SeekBar rotate1 = findViewById(R.id.rotateX);
+        SeekBar rotate2 = findViewById(R.id.rotateY);
+
+
+        rotate1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {img.setRotationX(seekBar.getProgress());}
+        });
+
+        rotate2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {img.setRotationY(seekBar.getProgress());}
+        });
     }
 }
